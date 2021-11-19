365aaed45bcfb9a59bdfa5d9636b4bbfda476ba6
yjh0410 <1394571815@qq.com>
2021-11-19 10:39:06 +0800

    update vit

--- backbone/ViT.py
+++ backbone/ViT.py
@@ -1,148 +1,88 @@
 import torch
-import numpy as np
 import torch.nn as nn
-from .transformer import build_encoder
-import math
-
-
-class Conv(nn.Module):
-    def __init__(self, c1, c2, k=1, p=0, s=1, d=1, g=1, act=True, bias=False):
-        super(Conv, self).__init__()
-        if act:
-            self.convs = nn.Sequential(
-                nn.Conv2d(c1, c2, k, stride=s, padding=p, dilation=d, groups=g, bias=bias),
-                nn.BatchNorm2d(c2),
-                nn.ReLU(inplace=True)
-            )
-        else:
-            self.convs = nn.Sequential(
-                nn.Conv2d(c1, c2, k, stride=s, padding=p, dilation=d, groups=g, bias=bias),
-                nn.BatchNorm2d(c2)
-            )
-
-    def forward(self, x):
-        return self.convs(x)
+from transformer import TransformerEncoder
 
 
 class ViT(nn.Module):
     def __init__(self, 
-                 args,
+                 img_size=224,
+                 patch_size=16,
+                 hidden_dim=256,
+                 num_heads=8,
+                 depth=6,
+                 mlp_dim=2048,
+                 dropout=0.,
+                 embed_dropout=0.,
+                 pool='cls',
                  num_classes=1000):
         super().__init__()
         self.num_classes = num_classes
-        # position embedding
-        self.pos_embed = self.position_embedding(hs=args["img_size"]//16, ws=args["img_size"]//16, 
-                                                    num_pos_feats=args["hidden_dim"]//2, normalize=True)
-
+        num_patches = (img_size // patch_size) ** 2
         # patch embedding
         self.patch_embedding = nn.Sequential(
-            Conv(3, args["hidden_dim"], k=16, s=16, act=False),
-            Conv(args["hidden_dim"], args["hidden_dim"], k=7, p=3, s=1)
+            nn.Conv2d(3, hidden_dim, kernel_size=patch_size, stride=patch_size),
+            nn.Conv2d(hidden_dim, hidden_dim, kernel_size=7, padding=3, stride=1)
         ) 
         
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, hidden_dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, hidden_dim))
+        self.dropout = nn.Dropout(embed_dropout)
+        
         # transformer encoder
-        self.encoder = build_encoder(args)
+        self.encoder = TransformerEncoder(dim=hidden_dim,
+                                          depth=depth,
+                                          heads=num_heads,
+                                          dim_head=hidden_dim // num_heads,
+                                          mlp_dim=mlp_dim,
+                                          dropout=dropout)
 
+        self.pool = pool
+        self.to_latent = nn.Identity()
         # output
-        self.fc = nn.Linear(args["hidden_dim"], num_classes)
+        self.out = nn.Sequential(
+            nn.LayerNorm(hidden_dim),
+            nn.Linear(hidden_dim, num_classes)
+        ) 
     
         
-    # Position Embedding
-    def position_embedding(self, hs=7, ws=7, num_pos_feats=128, temperature=10000, normalize=False, scale=None):
-        
-        if scale is not None and normalize is False:
-            raise ValueError("normalize should be True if scale is passed")
-        if scale is None:
-            scale = 2 * math.pi
-        # generate xy coord mat
-        # y_embed = [[0, 0, 0, ...], [1, 1, 1, ...]...]
-        # x_embed = [[0, 1, 2, ...], [0, 1, 2, ...]...]
-        y_embed, x_embed = torch.meshgrid([torch.arange(1, hs+1, dtype=torch.float32), 
-                                        torch.arange(1, ws+1, dtype=torch.float32)])
-        if normalize:
-            eps = 1e-6
-            y_embed = y_embed / (hs + eps) * scale
-            x_embed = x_embed / (ws + eps) * scale
-    
-        # [H, W] -> [1, H, W]
-        y_embed = y_embed[None, :, :]
-        x_embed = x_embed[None, :, :]
-
-
-        dim_t = torch.arange(num_pos_feats, dtype=torch.float32)
-        # torch.div(a, b, rounding_mode='floor') == (a // b)
-        dim_t_ = torch.div(dim_t, 2, rounding_mode='floor') / num_pos_feats
-        dim_t = temperature ** (2 * dim_t_)
-
-        pos_x = torch.div(x_embed[:, :, :, None], dim_t)
-        pos_y = torch.div(y_embed[:, :, :, None], dim_t)
-        pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
-        pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
-        # [B, d, H, W]
-        pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
-        
-        return pos
-
-
     def forward(self, x):
         # patch embedding
         x = self.patch_embedding(x)
-        x = x.flatten(2).permute(2, 0, 1)
-        pos_embed = self.pos_embed.flatten(2).permute(2, 0, 1).to(x.device)
+        # [B, C, H, W] -> [B, N, C]
+        x = x.flatten(2).permute(0, 2, 1)
+        B, N, _ = x.shape
+
+        cls_tokens = self.cls_token.repeat(B, 1, 1)
+        x = torch.cat([cls_tokens, x], dim=1)
+        x += self.pos_embedding
 
         # transformer
-        x = self.encoder(src=x, pos=pos_embed)
-        # [N, B, C] -> [B, N, C]
-        x = x.permute(1, 0, 2)
+        x = self.encoder(x)
 
         # [B, N, C] -> [B, C]
-        x = x.mean(1)
-        x = self.fc(x)
+        x = x.mean(1) if self.pool == 'mean' else x[:, 0]
+        x = self.to_latent(x)
 
-        return x
+        return self.out(x)
 
 
-def vit_256_6(pretrained=False, **kwargs):
-    args={
-        "img_size": 224,
-        "hidden_dim": 256,
-        "dropout": 0.1,
-        "num_heads": 8,
-        "mlp_dim": 2048,
-        "num_encoders": 6,
-        "pre_norm": False,
-    }
-    model = ViT(args=args)
-    return model
+def vit(pretrained=False, **kwargs):
+    model = ViT(img_size=224,
+                patch_size=16,
+                hidden_dim=256,
+                num_heads=8,
+                depth=6,
+                mlp_dim=2048,
+                dropout=0.1,
+                embed_dropout=0.1,
+                pool='cls',
+                num_classes=1000)
 
-def vit_256_12(pretrained=False, **kwargs):
-    args={
-        "img_size": 224,
-        "hidden_dim": 256,
-        "dropout": 0.1,
-        "num_heads": 8,
-        "mlp_dim": 2048,
-        "num_encoders": 12,
-        "pre_norm": False,
-    }
-    model = ViT(args=args)
-    return model
-
-def vit_512_12(pretrained=False, **kwargs):
-    args={
-        "img_size": 224,
-        "hidden_dim": 512,
-        "dropout": 0.1,
-        "num_heads": 8,
-        "mlp_dim": 2048,
-        "num_encoders": 12,
-        "pre_norm": False,
-    }
-    model = ViT(args=args)
     return model
 
 
 if __name__ == "__main__":
     x = torch.randn(2, 3, 224, 224)
-    model = vit_256_6()
+    model = vit()
     y = model(x)
+    print(y.size())
--- backbone/transformer.py
+++ backbone/transformer.py
@@ -1,131 +1,98 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-"""
-DETR Transformer class.
-
-Copy-paste from torch.nn.Transformer with modifications:
-    * positional encodings are passed in MHattention
-    * extra LN at the end of encoder is removed
-    * decoder returns a stack of activations from all decoding layers
-"""
-import copy
-from typing import Optional
-
 import torch
-import torch.nn.functional as F
-from torch import nn, Tensor
+import torch.nn as nn
 
 
+# PreNorm
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(self.norm(x))
+
+
+# FFN
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout=0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+
+# MultiHeadAttention
+class MultiHeadAttention(nn.Module):
+    def __init__(self, 
+                 dim,
+                 heads=8, 
+                 dim_head = 64,
+                 dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.dim_head = dim_head
+        self.heads = heads
+        self.scale = dim_head ** -0.5     # 1 / sqrt(d_k)
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        # [B, N, h*d] -> [B, N, h, d] -> [B, h, N, d]
+        q, k, v = self.to_qkv(x).chunk(3, dim=-1)
+        B, NQ = q.shape[:2]
+        B, NK = k.shape[:2]
+        B, NV = v.shape[:2]
+        q = q.view(B, NQ, self.heads, self.dim_head).permute(0, 2, 1, 3).contiguous()
+        k = k.view(B, NK, self.heads, self.dim_head).permute(0, 2, 1, 3).contiguous()
+        v = v.view(B, NV, self.heads, self.dim_head).permute(0, 2, 1, 3).contiguous()
+
+        # Q*K^T / sqrt(d_k) : [B, h, N, d] X [B, h, d, N] = [B, h, N, N]
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+        # softmax
+        attn = self.attend(dots)
+        # softmax(Q*K^T / sqrt(d_k)) * V ：[B, h, N, N] X [B, h, N, d] = [B, h, N, d]
+        out = torch.matmul(attn, v)
+        # [B, h, N, d] -> [B, N, h*d]=[B, N, C_out], C_out = h*d
+        out = out.permute(0, 2, 1, 3).contiguous().view(B, NQ, -1)
+        
+        return self.to_out(out)
+
+
+# Transformer Encoder Layer
 class TransformerEncoder(nn.Module):
-
-    def __init__(self, encoder_layer, num_layers, norm=None):
+    def __init__(self, 
+                 dim,            # hidden_dim
+                 depth,
+                 heads, 
+                 dim_head,
+                 mlp_dim=2048,
+                 dropout = 0.):
         super().__init__()
-        self.layers = _get_clones(encoder_layer, num_layers)
-        self.num_layers = num_layers
-        self.norm = norm
+        self.encoder_layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.encoder_layers.append(nn.ModuleList([
+                PreNorm(dim, MultiHeadAttention(dim, heads, dim_head, dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout))
+            ]))
 
-    def forward(self, src,
-                mask: Optional[Tensor] = None,
-                src_key_padding_mask: Optional[Tensor] = None,
-                pos: Optional[Tensor] = None):
-        output = src
-
-        for layer in self.layers:
-            output = layer(output, src_mask=mask,
-                           src_key_padding_mask=src_key_padding_mask, pos=pos)
-
-        if self.norm is not None:
-            output = self.norm(output)
-
-        return output
-
-
-class TransformerEncoderLayer(nn.Module):
-
-    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
-                 activation="relu", normalize_before=False):
-        super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
-        # Implementation of Feedforward model
-        self.linear1 = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout)
-        self.linear2 = nn.Linear(dim_feedforward, d_model)
-
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-
-        self.activation = _get_activation_fn(activation)
-        self.normalize_before = normalize_before
-
-    def with_pos_embed(self, tensor, pos: Optional[Tensor]):
-        return tensor if pos is None else tensor + pos
-
-    def forward_post(self,
-                     src,
-                     src_mask: Optional[Tensor] = None,
-                     src_key_padding_mask: Optional[Tensor] = None,
-                     pos: Optional[Tensor] = None):
-        q = k = self.with_pos_embed(src, pos)
-        src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
-        src = src + self.dropout1(src2)
-        src = self.norm1(src)
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
-        src = src + self.dropout2(src2)
-        src = self.norm2(src)
-        return src
-
-    def forward_pre(self, src,
-                    src_mask: Optional[Tensor] = None,
-                    src_key_padding_mask: Optional[Tensor] = None,
-                    pos: Optional[Tensor] = None):
-        src2 = self.norm1(src)
-        q = k = self.with_pos_embed(src2, pos)
-        src2 = self.self_attn(q, k, value=src2, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
-        src = src + self.dropout1(src2)
-        src2 = self.norm2(src)
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src2))))
-        src = src + self.dropout2(src2)
-        return src
-
-    def forward(self, src,
-                src_mask: Optional[Tensor] = None,
-                src_key_padding_mask: Optional[Tensor] = None,
-                pos: Optional[Tensor] = None):
-        if self.normalize_before:
-            return self.forward_pre(src, src_mask, src_key_padding_mask, pos)
-        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
-
-
-def _get_clones(module, N):
-    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
-
-
-def build_encoder(args):
-    encoder_layer = TransformerEncoderLayer(
-                        d_model=args["hidden_dim"],
-                        nhead=args["num_heads"],
-                        dim_feedforward=args["mlp_dim"],
-                        dropout=args["dropout"],
-                        activation="relu",
-                        normalize_before=args["pre_norm"])
-
-    encoder = TransformerEncoder(
-                    encoder_layer=encoder_layer,
-                    num_layers=args["num_encoders"],
-                    norm=nn.LayerNorm(args["hidden_dim"]))
-
-    return encoder
-
-
-def _get_activation_fn(activation):
-    """Return an activation function given a string"""
-    if activation == "relu":
-        return F.relu
-    if activation == "gelu":
-        return F.gelu
-    if activation == "glu":
-        return F.glu
-    raise RuntimeError(F"activation should be relu/gelu, not {activation}.")
+    def forward(self, x):
+        # x -> [B, N, d_in]
+        for attn, ffn in self.encoder_layers:
+            x = attn(x) + x
+            x = ffn(x) + x
+        return x
